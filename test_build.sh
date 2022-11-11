282d7353d6734d5ef117941196d6e296a7a0e789
KarolPlonka <karolplonka00@gmail.com>
2022-11-11 20:50:40 +0100

    with bug 3

--- test_build.sh
+++ test_build.sh
@@ -1,3 +1,8 @@
 docker run -d -p 80:80 karolplonka/nodejs_api:!insert_tag!; #run the container in background
 sleep 5; #wait 5 seconds
-curl 127.0.0.1/user/KarolPlonka; #test the get requset 
+
+if curl 127.0.0.1/user/KarolPlonka; then
+    exit 0
+else
+    exit 1
+fi
