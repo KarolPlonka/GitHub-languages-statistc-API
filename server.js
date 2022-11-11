7cdcaf9c1b0b27675318c3dacce70b35923513fe
KarolPlonka <karolplonka00@gmail.com>
2022-11-11 20:27:36 +0100

    curl test

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.get('/', (req, res) => {
     })
 })
 
-app.get('(/frac)?/userr/:id', (req, res) => {
+app.get('(/frac)?/user/:id', (req, res) => {
+    res.status(200).send({"pyhon": 1})
+    return
     /* GET DATA */
     console.log("request made for " + req.params.id)
     getReposName(req.params.id)
